27f967d6ae0f3047b550c8be629671576ad40b72
Alexis Louie <alouie1020@gmail.com>
2019-04-27 15:26:23 -0400

    move fetchResults function to SearchForm
    Previously, fetchResults would only be trigged when App was updated. However, it makes more sense for fetchResults to run after the SearchForm updated. This will avoid any confusion in case the App component becomes more complicated in the future.
    
    The SearchForm's input values are only relevant to that component as well, so it makes sense to keep those inputs within the component and only pass the results up to App.js.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { API_BASE_URL } from '../config';
 
 export default class SearchFrom extends React.Component {
     constructor(props) {
@@ -6,33 +7,53 @@ export default class SearchFrom extends React.Component {
         this.state = {
             query: '',
             location: '',
-            radius: ''
+            radius: '',
+            results: [],
         };
-    
+
         this.onSubmit = this.onSubmit.bind(this);
         this.handleQuery = this.handleQuery.bind(this);
         this.handleLocation = this.handleLocation.bind(this);
         this.handleRadius = this.handleRadius.bind(this);
-      }
-    
+    }
+
+    componentDidUpdate() {
+        this.fetchResults();
+    }
+
+    fetchResults() {
+        const { query, location, radius, results } = this.state;
+
+        fetch(`${API_BASE_URL}/findPlaces?query=${query}&location=${location}&radius=${radius}`,
+            { method: 'GET' })
+            .then(res => {
+                console.log('fetching data')
+                if (!res.ok) {
+                    return Promise.reject(res.statusText);
+                }
+                return res.json();
+            })
+            .then(data => {
+                this.setState({ results: data });
+            })
+            .then(() => this.props.onFormSubmit(results))
+    }
+
     onSubmit(event) {
         event.preventDefault();
-        this.props.onQueryChange(this.state.query);
-        this.props.onLocationChange(this.state.location);
-        this.props.onRadiusChange(this.state.radius);
     }
 
     handleQuery(event) {
         event.preventDefault();
-        this.setState({query: event.target.value});
+        this.setState({ query: event.target.value });
     }
     handleLocation(event) {
         event.preventDefault();
-        this.setState({location: event.target.value});
+        this.setState({ location: event.target.value });
     }
     handleRadius(event) {
         event.preventDefault();
-        this.setState({radius: event.target.value});
+        this.setState({ radius: event.target.value });
     }
 
     render() {
@@ -41,23 +62,23 @@ export default class SearchFrom extends React.Component {
                 <label>
                     What are you looking for?:
                     <br />
-                    <input type="text" name="query" placeholder="cafes, grocery stores, etc" 
-                        onChange={this.handleQuery}/>
+                    <input type="text" name="query" placeholder="cafes, grocery stores, etc"
+                        onChange={this.handleQuery} />
                 </label>
                 <br />
                 <br />
                 <label>
                     Location:
                     <br />
-                    <input type="text" name="location" placeholder="address" 
-                        onChange={this.handleLocation}/>
+                    <input type="text" name="location" placeholder="address"
+                        onChange={this.handleLocation} />
                 </label>
                 <br />
                 <br />
                 <label>
                     Radius (in meters):
                     <br />
-                    <input type="number" name="radius" 
+                    <input type="number" name="radius"
                         onChange={this.handleRadius} />
                 </label>
                 <input type="submit" value="Submit" />
