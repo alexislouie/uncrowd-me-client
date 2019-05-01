47e84942d98454ca06d9298cd2ae38e893640856
Alexis Louie <alouie1020@gmail.com>
2019-04-30 23:05:36 -0400

    render Results and pass in results as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import SearchForm from './SearchForm';
+import Results from './Results';
 
 export default class App extends Component {
   constructor(props) {
@@ -19,6 +19,8 @@ export default class App extends Component {
   }
 
   render() {
+    const { results } = this.state;
+
     return (
       <div className="App">
         <h1>Uncrowd Me</h1>
@@ -26,6 +28,7 @@ export default class App extends Component {
           onFormSubmit={results => {
             this.onFormSubmit(results)
           }} />
+        <Results results={results} />
       </div>
     );
   }
