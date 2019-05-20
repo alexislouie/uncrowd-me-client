a76326270d6e3e079ac1f572bf4daa455c7771f5
Alexis Louie <alouie1020@gmail.com>
2019-05-20 14:14:26 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+export default function Home(props) {
+    return (
+        <div className="home">
+            <main>
+                <div className="homePage">
+                    <h2>Uncrowd Me.</h2>
+                    <button className="learnMoreButton">Learn more</button>
+                </div>
+                <div className="about">
+                    <h2>Living in the city doesn't always have to feel crowded.</h2>
+                    <p>
+                        We've all experienced it: the elbow-to-elbow crowds at your favorite grocery store on the weekends. Long lines that wrap around the store that only seem to clear up as soon as you've checked out.
+                        Realizing there are no seats when you show up at a cafe to get some work done. 
+                    </p>
+                    <p>
+                        Avoid the crowds and avoid the wait. Uncrowd Me shows you the closest places with the smallest crowds near you. 
+                    </p>
+                </div>
+            </main>
+        </div>
+    );
+}
