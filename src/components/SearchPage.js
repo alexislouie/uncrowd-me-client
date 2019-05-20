6c0533e39c08456fcc230a8f34a217749b596496
Alexis Louie <alouie1020@gmail.com>
2019-05-20 14:14:53 -0400

    move App.js to SearchPage

---
+++ src/components/SearchPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import SearchForm from './SearchForm';
+import Results from './Results';
+
+export default class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: []
+    };
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(results) {
+    this.setState({
+      results
+    });
+  }
+
+  render() {
+    const { results } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Uncrowd Me</h1>
+        <SearchForm
+          onFormSubmit={results => {
+            this.onFormSubmit(results)
+          }} />
+        <Results results={results} />
+      </div>
+    );
+  }
+}
