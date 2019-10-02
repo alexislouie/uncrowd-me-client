73f30a451cbc49e232c3adc542a07d1ffe612c9a
Alexis Louie <alexislouie@Alexiss-MacBook-Pro.local>
2019-10-02 18:34:58 -0400

    add SVG markers

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -5,32 +5,25 @@ export default class MapMarker extends Component {
 
     handleInfoBox() {
         const { name, address, lat, lng } = this.props;
-        this.props.showInfoBox(name, address, lat, lng)
+        this.props.showInfoBox(name, address, lat, lng);
     }
 
     render() {
-        const markerStyle = {
-            width: '7px',
-            height: '7px',
-            backgroundColor: 'blue'
-        };
-
         const styleHover = {
-            width: '10px',
-            height: '10px',
-            backgroundColor: 'blue',
-            border: '2px solid green'
+            fill: 'pink',
+            stroke: 'white',
+            strokeWidth: '2px'
         };
 
         const { $hover, $dimensionKey, userLocation, updateHoveredMarker, hoveredCardId, currentHover, text, index } = this.props;
 
-        let style; 
+        let style;
         if ($hover && !userLocation) {
             style = styleHover;
             updateHoveredMarker($dimensionKey);
-        } 
+        }
         else {
-            style = markerStyle;
+            style = null;
         }
 
         if ($dimensionKey === currentHover && !$hover) {
@@ -39,17 +32,25 @@ export default class MapMarker extends Component {
 
         if (hoveredCardId === $dimensionKey) {
             style = styleHover;
-        } 
+        }
 
         if (userLocation) {
             return (
-                <div className="marker userLocation">
+                <div className="userLocation">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="marker userPin">
+                        <path class="st0" d="M25.2 2c-7.5 0-13.5 6.1-13.5 13.5 0 7.2 13.1 30.6 13.5 32.4 0.5-1.7 13.5-25.1 13.5-32.3C38.7 8.1 32.7 2 25.2 2zM25.5 20c-3 0-5.5-2.5-5.5-5.5 0-3 2.5-5.5 5.5-5.5s5.5 2.5 5.5 5.5C31 17.5 28.5 20 25.5 20z" />
+                        <path class="st0" d="M25.2 48c0 0 0 0 0-0.1C25.2 48 25.2 48 25.2 48z" />
+                    </svg>
                     {text}
                 </div>
             )
         } else {
             return (
-                <div className="marker placeMarker" index={index} style={style} onClick={() => this.handleInfoBox()}>
+                <div className="placeMarker" index={index} style={style} onClick={() => this.handleInfoBox()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="marker placePin" style={style}>
+                        <path class="st0" d="M25.2 2c-7.5 0-13.5 6.1-13.5 13.5 0 7.2 13.1 30.6 13.5 32.4 0.5-1.7 13.5-25.1 13.5-32.3C38.7 8.1 32.7 2 25.2 2zM25.5 20c-3 0-5.5-2.5-5.5-5.5 0-3 2.5-5.5 5.5-5.5s5.5 2.5 5.5 5.5C31 17.5 28.5 20 25.5 20z" />
+                        <path class="st0" d="M25.2 48c0 0 0 0 0-0.1C25.2 48 25.2 48 25.2 48z" />
+                    </svg>
                 </div>
             )
         }
