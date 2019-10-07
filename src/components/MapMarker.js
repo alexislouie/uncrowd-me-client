0cbde412dea4a75559e6edcfbf94757dcade40eb
Alexis Louie <alexislouie@Alexiss-MacBook-Pro.local>
2019-10-06 22:30:41 -0400

    add marker styling/animation

--- src/components/MapMarker.js
+++ src/components/MapMarker.js
@@ -3,9 +3,9 @@ import './MapMarker.css';
 
 export default class MapMarker extends Component {
 
-    handleInfoBox() {
-        const { name, address, lat, lng } = this.props;
-        this.props.showInfoBox(name, address, lat, lng);
+    handleClick() {
+        const { name, address, lat, lng, $dimensionKey } = this.props;
+        this.props.showInfoBox(name, address, lat, lng, $dimensionKey);
     }
 
     render() {
@@ -15,9 +15,11 @@ export default class MapMarker extends Component {
             strokeWidth: '2px'
         };
 
-        const { $hover, $dimensionKey, userLocation, updateHoveredMarker, hoveredCardId, currentHover, text, index } = this.props;
+        const { $hover, $dimensionKey, userLocation, updateHoveredMarker, hoveredCardId, currentHover, text, index, clickedMarker } = this.props;
 
         let style;
+
+        // don't want to allow hover style change for userLocation
         if ($hover && !userLocation) {
             style = styleHover;
             updateHoveredMarker($dimensionKey);
@@ -36,8 +38,8 @@ export default class MapMarker extends Component {
 
         if (userLocation) {
             return (
-                <div className="userLocation">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="marker userPin">
+                <div className="marker userLocation">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="userPin">
                         <path class="st0" d="M25.2 2c-7.5 0-13.5 6.1-13.5 13.5 0 7.2 13.1 30.6 13.5 32.4 0.5-1.7 13.5-25.1 13.5-32.3C38.7 8.1 32.7 2 25.2 2zM25.5 20c-3 0-5.5-2.5-5.5-5.5 0-3 2.5-5.5 5.5-5.5s5.5 2.5 5.5 5.5C31 17.5 28.5 20 25.5 20z" />
                         <path class="st0" d="M25.2 48c0 0 0 0 0-0.1C25.2 48 25.2 48 25.2 48z" />
                     </svg>
@@ -46,8 +48,8 @@ export default class MapMarker extends Component {
             )
         } else {
             return (
-                <div className="placeMarker" index={index} style={style} onClick={() => this.handleInfoBox()}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className="marker placePin" style={style}>
+                <div className="marker placeMarker" index={index} style={style} onClick={() => this.handleClick()}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" className={` placePin ${(clickedMarker === $dimensionKey) ? "jump selected" : ""}`} style={style}>
                         <path class="st0" d="M25.2 2c-7.5 0-13.5 6.1-13.5 13.5 0 7.2 13.1 30.6 13.5 32.4 0.5-1.7 13.5-25.1 13.5-32.3C38.7 8.1 32.7 2 25.2 2zM25.5 20c-3 0-5.5-2.5-5.5-5.5 0-3 2.5-5.5 5.5-5.5s5.5 2.5 5.5 5.5C31 17.5 28.5 20 25.5 20z" />
                         <path class="st0" d="M25.2 48c0 0 0 0 0-0.1C25.2 48 25.2 48 25.2 48z" />
                     </svg>
