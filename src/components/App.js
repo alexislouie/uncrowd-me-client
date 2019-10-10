30ccdaee747d9a42e442ba0adbb678cb6beae859
Alexis Louie <alexislouie@Alexiss-MacBook-Pro.local>
2019-10-10 18:02:31 -0400

    add TransitionGroup and CSSTransition

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import SearchForm from './SearchForm';
 import SearchPage from './SearchPage';
 import Results from './Results';
 import Home from './Home';
+import './App.css'
 // import LoginPage from './LoginPage';
+import {
+    CSSTransition,
+    TransitionGroup,
+} from 'react-transition-group';
+
 
 export default function App(props) {
-  return (
-      <Router>
-          <div className="app">
-              {/* <header>
-                  <h1><Link to="/">Uncrowd Me</Link></h1>
-              </header> */}
-              <main>
-                  <Route exact path="/" component={Home} />
-                  <Route exact path="/search" component={SearchPage} />
-                  <Route exact path="/search/:query/:address" components={{searchForm: SearchForm, main: Results}} />
-                  {/* <Route exact path="/" component={LoginPage} /> */}
-                  <Link to="/search">Search</Link>
-              </main>
-          </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <Route render={({ location }) => (
+
+                <div className="app">
+                    <header>
+                        <h1><Link to="/">Uncrowd Me</Link></h1>
+                    </header>
+                    <main>
+                        <TransitionGroup>
+                            <CSSTransition
+                                key={location.key}
+                                classNames="fade"
+                                timeout={300}
+                            >
+                                <Switch location={location}>
+                                    <Route exact path="/" component={Home} />
+                                    <Route exact path="/search" component={SearchPage} />
+                                </Switch>
+                            </CSSTransition>
+                        </TransitionGroup>
+                    </main>
+                </div>
+            )} />
+
+        </Router>
+    );
 }
