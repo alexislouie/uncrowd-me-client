dcf38ad00bb31980b4af487ac98d88d8daf9db33
Alexis Louie <alexislouie@Alexiss-MacBook-Pro.local>
2019-10-27 19:04:07 -0400

    remove unnecessary code

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,48 +1,13 @@
-// import React, {Component} from 'react';
-// import './InfoBox.css';
-
-
-// export default class InfoBox extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             hidden: false
-//         };
-//     }
-
-//     componentDidUpdate(prevProps) {
-//         if (prevProps.key === this.props.key) {
-//             this.setState({hidden: true})
-//         }
-//     }
-
-//     render() {
-//         const { name, address } = props;
-
-//         const style = name ? { display: 'block' } : { display: 'none' };
-//         const view = (props.hide === true) ? 'hide' : 'show';
-    
-//         return (
-//             <div className={`infoBox ${view}`} style={style}>
-//                 {name}<br />
-//                 {address}
-//             </div>
-//         )
-//     }
-// }
-
 import React from 'react';
 import './InfoBox.css';
 
 
 export default function InfoBox(props) {
     const { name, address } = props;
-
     const style = name ? { display: 'block' } : { display: 'none' };
-    const view = (props.hide === true) ? 'hide' : 'show';
 
     return (
-        <div className={`infoBox ${view}`} style={style}>
+        <div className='infobox' style={style}>
             {name}<br />
             {address}
         </div>
